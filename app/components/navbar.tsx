7a6ee31be892c23ec04b6a255e32253e378938d3
Andri10dev <andri10.dev@gmail.com>
2023-10-27 23:28:20 +0700

    update responsive navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -9,109 +9,6 @@ import React, { useEffect, useState } from 'react'
 import { faClose } from '@fortawesome/free-solid-svg-icons/faClose';
 
 const Navbar = () => {
-
-    const [activeLink, setActiveLink] = useState('');
-    const scrollToSection = (sectionId: any) => {
-        const element = document.getElementById(sectionId);
-        if (element) {
-            element.scrollIntoView({ behavior: "smooth" });
-        }
-
-        var active = 'font-semibold text-blue-500';
-        var inActive = 'font-semibold text-white';
-
-        var btnName = 'btnHome';
-        const btnHome = document.getElementById('btnHome');
-        const btnAbout = document.getElementById('btnAbout');
-        const btnService = document.getElementById('btnService');
-        const btnSkill = document.getElementById('btnSkill');
-        const btnPortFolio = document.getElementById('btnPortFolio');
-        const btnBlog = document.getElementById('btnBlog');
-        const btnContact = document.getElementById('btnContact');
-
-        switch (sectionId) {
-            case 'welcomeSection':
-                btnHome!.className = active;
-                btnAbout!.className = inActive;
-                btnService!.className = inActive;
-                btnSkill!.className = inActive;
-                btnPortFolio!.className = inActive;
-                btnBlog!.className = inActive;
-                btnContact!.className = inActive;
-                break;
-            case 'aboutSection':
-                btnHome!.className = inActive
-                btnAbout!.className = active;
-                btnService!.className = inActive;
-                btnSkill!.className = inActive;
-                btnPortFolio!.className = inActive;
-                btnBlog!.className = inActive;
-                btnContact!.className = inActive;
-                break;
-            case 'serviceSection':
-                btnHome!.className = inActive
-                btnAbout!.className = inActive;
-                btnService!.className = active
-                btnSkill!.className = inActive;
-                btnPortFolio!.className = inActive;
-                btnBlog!.className = inActive;
-                btnContact!.className = inActive;
-                break;
-
-            case 'personalSkillSection':
-                btnHome!.className = inActive
-                btnAbout!.className = inActive;
-                btnService!.className = inActive;
-                btnSkill!.className = active;
-                btnPortFolio!.className = inActive;
-                btnBlog!.className = inActive;
-                btnContact!.className = inActive;
-                break;
-
-            case 'portfolioSection':
-                btnHome!.className = inActive
-                btnAbout!.className = inActive;
-                btnService!.className = inActive;
-                btnSkill!.className = inActive;
-                btnPortFolio!.className = active;
-                btnBlog!.className = inActive;
-                btnContact!.className = inActive;
-                break;
-
-            case 'blogSection':
-                btnHome!.className = inActive
-                btnAbout!.className = inActive;
-                btnService!.className = inActive;
-                btnSkill!.className = inActive;
-                btnPortFolio!.className = inActive;
-                btnBlog!.className = active;
-                btnContact!.className = inActive;
-                break;
-
-            case 'footer':
-                btnHome!.className = inActive
-                btnAbout!.className = inActive;
-                btnService!.className = inActive;
-                btnSkill!.className = inActive;
-                btnPortFolio!.className = inActive;
-                btnBlog!.className = inActive;
-                btnContact!.className = active;
-                break;
-
-
-            default:
-                btnHome!.className = active;
-                btnAbout!.className = inActive;
-                btnService!.className = inActive;
-                btnSkill!.className = inActive;
-                btnPortFolio!.className = inActive;
-                btnBlog!.className = inActive;
-                btnContact!.className = inActive;
-                break;
-
-        }
-    }
-
     const handleScroll = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
         // first prevent the default behavior
         e.preventDefault();
@@ -125,10 +22,14 @@ const Navbar = () => {
         });
     };
 
+    const [isCollapse, setIsCollapse] = useState(true);
+    const toggleCollapse = () => {
+        setIsCollapse(!isCollapse)
+    }
     return (
         <>
-            <nav className="w-full bg-black/50 backdrop-blur-sm text-white fixed z-50">
-                <div className="w-full h-16 flex items-center justify-between px-5 lg:px-10">
+            <nav className="w-full text-white fixed z-50">
+                <div className="bg-black/50 backdrop-blur-sm w-full h-16 flex items-center justify-between px-5 lg:px-10">
                     <div className="font-semibold">
                         Andri Ardiansyah
                     </div>
@@ -141,10 +42,20 @@ const Navbar = () => {
                         <Link href="#blogSection" onClick={handleScroll}>Blog</Link>
                         <Link href="#footer" onClick={handleScroll}>Contact</Link>
                     </div>
-                    <button type='button' className='w-14 aspect-square bg-black flex items-center justify-center text-2xl'>
-                        <FontAwesomeIcon icon={faBars} />
-                        <FontAwesomeIcon icon={faClose} className='absolute text-red-500'/>
+                    <button type='button' onClick={toggleCollapse} className=' flex items-center justify-center text-2xl relative'>
+                        <FontAwesomeIcon icon={faBars} className={isCollapse ? 'scale-100 rotate-0 transition-all duration-500' : 'scale-0 rotate-180 transition-all duration-300'} />
+                        <FontAwesomeIcon icon={faClose} className={isCollapse ? 'absolute scale-0 -rotate-180 transition-all duration-300' : 'absolute scale-100 text-red-500 rotate-0 transition-all duration-500'} />
                     </button>
+                </div>
+                <div className={isCollapse ? 'w-full h-0 px-5 flex flex-col items-center space-y-3 overflow-hidden transition-all duration-700 rounded-b-2xl bg-black/50 backdrop-blur-sm' : 'lg:hidden w-full px-5 flex flex-col items-center space-y-3 h-72 transition-all duration-700 overflow-hidden rounded-b-2xl bg-black/50 backdrop-blur-sm'}>
+                    <div className='w-full h-1 bg-white rounded-full mb-3'></div>
+                    <Link href="#welcomeSection" onClick={handleScroll}>Home</Link>
+                    <Link href="#aboutSection" onClick={handleScroll}>About</Link>
+                    <Link href="#serviceSection" onClick={handleScroll}>Service</Link>
+                    <Link href="#personalSkillSection" onClick={handleScroll}>Personal Skill</Link>
+                    <Link href="#portfolioSection" onClick={handleScroll}>Portfolio</Link>
+                    <Link href="#blogSection" onClick={handleScroll}>Blog</Link>
+                    <Link href="#footer" onClick={handleScroll}>Contact</Link>
                 </div>
             </nav>
 
